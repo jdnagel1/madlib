649558f7be4fb9a5f4452b78d44885e6f57f05ae
Alexander Kondra and Jake Nagel <jake@>
2019-03-27 10:07:10 -0700

    test madlib form and text

--- js/scripts.js
+++ js/scripts.js
@@ -1,131 +1,22 @@
-$(document).ready(function() {
-  $("p.showing1").click(function() {
-    $(".showing1").toggle();
-    $(".hidden1").toggle();
-  });
-});
-$(document).ready(function() {
-  $("p.showing2").click(function() {
-    $(".showing2").toggle();
-    $(".hidden2").toggle();
-  });
-});
 
 $(document).ready(function() {
-  $("p.showing3").click(function() {
-    $(".showing3").toggle();
-    $(".hidden3").toggle();
-  });
-});
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
 
-$(document).ready(function() {
-  $("p.showing4").click(function() {
-    $(".showing4").toggle();
-    $(".hidden4").toggle();
-  });
-});
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
-$(document).ready(function() {
-  $("p.showing5").click(function() {
-    $(".showing5").toggle();
-    $(".hidden5").toggle();
-  });
-});
+    $("#story").show();
 
-$(document).ready(function() {
-  $("p.showing6").click(function() {
-    $(".showing6").toggle();
-    $(".hidden6").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing7").click(function() {
-    $(".showing7").toggle();
-    $(".hidden7").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing8").click(function() {
-    $(".showing8").toggle();
-    $(".hidden8").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing9").click(function() {
-    $(".showing9").toggle();
-    $(".hidden9").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing10").click(function() {
-    $(".showing10").toggle();
-    $(".hidden10").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing11").click(function() {
-    $(".showing11").toggle();
-    $(".hidden11").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing12").click(function() {
-    $(".showing12").toggle();
-    $(".hidden12").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing13").click(function() {
-    $(".showing13").toggle();
-    $(".hidden13").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing14").click(function() {
-    $(".showing14").toggle();
-    $(".hidden14").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing15").click(function() {
-    $(".showing15").toggle();
-    $(".hidden15").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing16").click(function() {
-    $(".showing16").toggle();
-    $(".hidden16").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing17").click(function() {
-    $(".showing17").toggle();
-    $(".hidden17").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing18").click(function() {
-    $(".showing18").toggle();
-    $(".hidden18").toggle();
-  });
-});
-
-$(document).ready(function() {
-  $("p.showing19").click(function() {
-    $(".showing19").toggle();
-    $(".hidden19").toggle();
+    event.preventDefault();
   });
 });
